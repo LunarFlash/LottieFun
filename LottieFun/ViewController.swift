0e16674f6a3053d6e6fe87e53ea50b228f65ab98
Wang, Terry <terry.wang@accenture.com>
2017-06-17 01:23:43 -0400

    added some extra transform code

--- LottieFun/ViewController.swift
+++ LottieFun/ViewController.swift
@@ -14,11 +14,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let animationView = LOTAnimationView(name: "servishero_loading") {
+        if let animationView = LOTAnimationView(contentsOf: URL(string: "https://github.com/airbnb/lottie-ios/raw/master/Example/Assets/PinJump.json")!) {
             animationView.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
             animationView.center = self.view.center
             animationView.contentMode = .scaleAspectFill
             view.addSubview(animationView)
+            animationView.loopAnimation = true
+            animationView.animationSpeed = 0.5
+            
+            let rotateTransform = CGAffineTransform(rotationAngle: 45.0)
+            animationView.transform = rotateTransform
+            
+            // Applying UIView animation
+            let minimizeTransform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+            animationView.transform = minimizeTransform
+            UIView.animate(withDuration: 3.0, delay: 0.0, options: [.repeat, .autoreverse], animations: {
+                animationView.transform = CGAffineTransform.identity
+            }, completion: nil)
+            
+            
             animationView.play()
         }
     }
